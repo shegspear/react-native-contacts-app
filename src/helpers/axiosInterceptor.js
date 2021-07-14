f043e75b0695e30b54793670c728f28826781555
shegspear <shegspear@gmail.com>
2021-07-14 20:14:34 +0100

    initiated axios

---
+++ src/helpers/axiosInterceptor.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import envs from '../config/env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+let headers = {};
+
+const axiosInstance = axios.create({
+  baseURL: envs.BACKEND_URL,
+  headers  
+});
+
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = 'Bearer ${token}';
+        }
+
+        return config;
+    },
+     (errors) => {
+         return Promise.reject(error);
+     }
+);
+
+export default axiosInstance;
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,16 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 import RegisterComponent from '../../components/Signup/index';
 import envs from '../../config/env';
+import axiosInstance from '../../helpers/axiosInterceptor';
 
 const Register = () => {
  const [form, setForm] = useState({});
  const [errors, setErrors] = useState({});
- const {BACKEND_URL} = envs;
-
-  console.log('BACKEND_URL :>>', BACKEND_URL);
-  console.log('_DEV_', __DEV__);
+ 
+  useEffect(() => {
+    axiosInstance.post('/contacts').catch((err) => {
+     console.log('err', err.response);
+    });
+  }, []);
 
   const onChange = ({name, value}) => {
     setForm({...form, [name]: value});
